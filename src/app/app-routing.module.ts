d0e755044cc62b9f6d250cc8e95f0ac15be27eb1
ATrejo <apolinartrejo@utng.edu.mx>
2022-06-15 17:21:44 -0500

    practica-bootstrap-login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { RegistroComponent } from './components/registro/registro.component';
+import { LoginComponent } from './components/login/login.component';
+import { AppComponent } from './app.component';
+import { AsistentesComponent } from './components/asistentes/asistentes.component';
+import { ParticipantesComponent } from './components/participantes/participantes.component';
+import { AdministradorComponent } from './components/administrador/administrador.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'administrador',component:AdministradorComponent},
+  {path:'participantes',component:ParticipantesComponent},
+  {path:'asistentes',component:AsistentesComponent},
+  {path:'registro',component:RegistroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdministradorComponent } from './components/administrador/administrador.component';
+import { ParticipantesComponent } from './components/participantes/participantes.component';
+import { AsistentesComponent } from './components/asistentes/asistentes.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { PiePaginaComponent } from './components/pie-pagina/pie-pagina.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdministradorComponent,
+    ParticipantesComponent,
+    AsistentesComponent,
+    LoginComponent,
+    NavegacionComponent,
+    PiePaginaComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
